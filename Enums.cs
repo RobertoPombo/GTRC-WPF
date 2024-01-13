df963a956020d6c2c683e3e86a5313b808c3b7e9
RobertoPombo <captn.sponge2@web.de>
2024-01-13 18:31:30 +0100

    Added Enum DataDisplayType.

---
+++ Enums.cs
@@ -0,0 +1,11 @@
+﻿namespace GTRC_WPF
+{
+    public enum DataDisplayType
+    {
+        Default = 0,
+        Dropdown = 1,
+        DropdownId = 2,
+        Color = 3,
+        Image = 4
+    }
+}
