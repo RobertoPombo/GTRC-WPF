cb10e7a5437162578fd7f71f49e6a1f2d2e7f85a
RobertoPombo <captn.sponge2@web.de>
2024-08-19 22:21:44 +0200

    Moved DiscordMessageType to GTRC_Basics.Enums.

--- Enums.cs
+++ Enums.cs
@@ -18,18 +18,4 @@
         Run = 3,
         RunWait = 4
     }
-
-    public enum DiscordMessageType
-    {
-        PermanentInfo = 0,
-        Commands = 1,
-        Entries = 2,
-        NewEntries = 3,
-        SeasonSettingsViolations = 4,
-        BoP = 5,
-        Events = 6,
-        Cars = 7,
-        Organizations = 8,
-        Rating = 9
-    }
 }
